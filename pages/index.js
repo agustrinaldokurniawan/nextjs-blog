5bf59e2181e1af1be6ffce9bcc7de92ec8356bea
agustrinaldokurniawan <agustkurniawan010899@gmail.com>
2021-01-14 05:30:03 +0700

    index

--- pages/index.js
+++ pages/index.js
@@ -29,8 +29,8 @@ export default function Home({ allPostsData }) {
           is my power. So, dont take away keyboard from me :)
         </p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.
         </p>
       </section>{" "}
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
